9d50a8a0e3b2a071c94058269097fd4fb37a3734
Justin O'Boyle <justin@justinoboyle.com>
2022-10-03 20:41:59 -0400

    Add basic query

---
+++ web/lib/oapen/OAPENTypes.ts
@@ -0,0 +1,36 @@
+export type OAPENTypeID = string;
+export type OAPENHandle = string;
+export type OAPENType = string;
+export type OAPENExpandable = string;
+export type OAPENBitstreams = string;
+export type OAPENApiRelativePath = string;
+export type URL = string;
+
+export interface Expandable {
+  expand?: OAPENExpandable[];
+}
+
+export interface OAPENItemRaw {
+  uuid: OAPENTypeID;
+  name?: string;
+  handle?: OAPENHandle;
+  type?: OAPENType;
+  archived: boolean;
+  withdrawn: boolean;
+  link?: OAPENApiRelativePath;
+}
+
+export interface RawMetadataItem {
+  key: string;
+  value: string;
+  qualifier?: string;
+  language?: string;
+  element?: string;
+}
+
+type HasMetadata = {
+  metadata: OAPENMetadata;
+};
+
+export type OAPENItemQueried = OAPENItemRaw & Expandable;
+export type OAPENItemWithMetadata = OAPENItemQueried & HasMetadata;
--- web/pages/items/[uuid].tsx
+++ web/pages/items/[uuid].tsx
@@ -1,22 +1,37 @@
-import type { GetStaticProps } from "next";
+import type { GetStaticProps, GetStaticPaths } from "next";
+import { OAPENItems } from "../../lib/oapen";
+import type { OAPENItemWithMetadata } from "../../lib/oapen/OAPENTypes";
 
 interface SingleItemProps {
-  uuid: string;
+  item: OAPENItemWithMetadata;
 }
 
-export const SingleItem = ({ uuid }: SingleItemProps) => {
+export default function ItemSingle({ item }: SingleItemProps) {
+  const name = item.metadata.find(({ key }) => key == "grantor.name")?.value;
   return (
     <>
-      <h1>{uuid}</h1>
+      <h1>{name}</h1>
+      <code>
+        <pre>{JSON.stringify({ item }, null, 4)}</pre>
+      </code>
     </>
   );
+}
+
+// TODO update
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: "blocking", // can also be true or 'blocking
+  };
 };
 
 export const getStaticProps: GetStaticProps<SingleItemProps> = async (
   context
 ) => {
+  const item = await OAPENItems.getItemWithMetadata(context?.params?.uuid + "");
   const data: SingleItemProps = {
-    uuid: context.params?.uuid,
+    item,
   };
 
   return {
