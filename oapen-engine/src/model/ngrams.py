84933da8b5586892ea2592f9817088de1713217c
Max Zaremba <mzaremba@stevens.edu>
2022-10-17 13:48:12 -0400

    removed non-alpha characters (letters only)

--- oapen-engine/src/model/ngrams.py
+++ oapen-engine/src/model/ngrams.py
@@ -26,7 +26,7 @@ def process_text(text):
     l_text = text.lower()
     p_text = "".join([c for c in l_text if c not in string.punctuation])
     words = word_tokenize(p_text)
-    filtered_words = list(filter(lambda x: x not in STOPWORDS, words))
+    filtered_words = list(filter(lambda x: x not in STOPWORDS and x.isalpha(), words)) #added isalpha to check that it contains only letters
 
     return filtered_words
 
@@ -76,8 +76,8 @@ def get_n_most_occuring(dic, n=100):
     sorted_dict = dict(sorted(dic.items(), key=lambda item: item[1])) #sorts in case of additionas post generate_ngram
     return list(sorted_dict)[:n]
 
-#Currently, this uses the n most occuring ngrams to compare
-#This could also count the instances in the highest
+# Currently, this uses the n most occuring ngrams to compare
+# This could also count the instances in the highest
 def get_similarity_score(ngram1, ngram2, n=100):
     n_most_occ_1 = get_n_most_occuring(ngram1, n)
     n_most_occ_2 = get_n_most_occuring(ngram2, n)
@@ -86,6 +86,17 @@ def get_similarity_score(ngram1, ngram2, n=100):
         if n_gram in n_most_occ_2:
             repeated += 1
     return repeated/n
+
+# this treats ngrams1 as primary ngrams, since we want a 
+# 100% similarity score if all ngrams match from book 1
+# this means that a fragment of a book will get a 100% similarity score
+# when compared to it's own book, but not the reverse interaction
+def get_similarity_score_by_dict_count(ngrams1, ngrams2):
+    repeated = 0
+    total = sum(ngrams1.values()) #gets counts from book 1
+    for key, ngrams1_value in ngrams1.items():
+        repeated += min(ngrams1_value, ngrams2.get(key, 0)) #adds min value, or 0 by default if key not found
+    return repeated/total
 
 def run_ngrams():
     data = get_data()
