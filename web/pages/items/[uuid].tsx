089cb2bbc13086c7c36e52017cf0afa469935624
Justin O'Boyle <justin@justinoboyle.com>
2022-10-18 07:32:55 -0400

    Query SelfSuggestions from API

--- web/pages/items/[uuid].tsx
+++ web/pages/items/[uuid].tsx
@@ -1,10 +1,12 @@
 import type { GetStaticProps, GetStaticPaths } from "next";
 import { OAPENItems } from "../../lib/oapen";
+import { SelfItems } from "../../lib/self";
 import type { OAPENItemWithMetadata } from "../../lib/oapen/OAPENTypes";
 import { RenderItem } from "../../components/render/RenderItem";
 
 interface SingleItemProps {
   item: OAPENItemWithMetadata;
+  selfSuggestions: any; // TODO change
 }
 
 export default function ItemSingle({ item }: SingleItemProps) {
@@ -30,9 +32,18 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps<SingleItemProps> = async (
   context
 ) => {
+  // TODO write async
   const item = await OAPENItems.getItemWithMetadata(context?.params?.uuid + "");
+  let selfSuggestions = {};
+  try {
+    selfSuggestions = await SelfItems.getItemByHandle(item?.handle || "");
+  } catch (e) {
+    console.error("Could not fetch SelfSuggestions", e);
+  }
+
   const data: SingleItemProps = {
     item,
+    selfSuggestions,
   };
 
   return {
