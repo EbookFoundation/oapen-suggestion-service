cead31178d99e8a2835b9d3781415935bbf62c38
Justin O'Boyle <justin@justinoboyle.com>
2022-11-08 08:10:16 -0500

    Remove todo

--- api/validate.js
+++ api/validate.js
@@ -11,7 +11,6 @@ let checkHandle = async (handle) => {
   if(!handle) throw new UserError("Handle must not be null")
   if(typeof handle !== "string") throw new UserError("Handle must be a string")
   if(!handle.includes("/")) throw new UserError("Invalid handle format");
-  // TODO: Validate the book's handle
   return true;
 };
 
