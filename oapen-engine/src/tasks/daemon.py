f48511f54af54764f54d64121d64e822aca19f9d
Justin O'Boyle <justin@justinoboyle.com>
2022-12-13 07:43:25 -0500

    Make Black Formatter happy?

--- oapen-engine/src/tasks/daemon.py
+++ oapen-engine/src/tasks/daemon.py
@@ -1,9 +1,12 @@
 # Daemon to run processes in the background
 import time
 import sys, signal
+
+
 def signal_handler(signal, frame):
     print("\nprogram exiting gracefully")
     sys.exit(0)
+
 
 signal.signal(signal.SIGINT, signal_handler)
 
