da8fbfe1dbb7262793c001ecba8734fab0a9942c
eric <eric@hellman.net>
2023-04-17 17:13:34 -0400

    move password inside TLS envelope

--- api/db/connection.js
+++ api/db/connection.js
@@ -21,7 +21,7 @@ if (
     "Some Postgres environment variables weren't found. Please configure them in the .env file."
   );
 
-const connection_string = `postgresql://${process.env.POSTGRES_USERNAME}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB_NAME}?sslmode=${process.env.POSTGRES_SSLMODE}`;
+const connection_string = `postgresql://${process.env.POSTGRES_USERNAME}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB_NAME}?sslmode=${process.env.POSTGRES_SSLMODE}&password=${process.env.POSTGRES_PASSWORD}`;
 const db = pgp(connection_string);
 
 module.exports = db;
