0ed777f8ce509adf80a4b85d57d8b7c177220375
Peter Rauscher <peterrauscher@protonmail.com>
2023-04-18 10:50:35 -0400

    Fix for sslmode in API connection string

--- api/db/connection.js
+++ api/db/connection.js
@@ -16,12 +16,11 @@ if (
     process.env.POSTGRES_DB_NAME &&
     process.env.POSTGRES_SSLMODE
   )
-)
-  throw new DatabaseConnectionError(
-    "Some Postgres environment variables weren't found. Please configure them in the .env file."
-  );
-
-const connection_string = `postgresql://${process.env.POSTGRES_USERNAME}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB_NAME}?sslmode=${process.env.POSTGRES_SSLMODE}`;
+) throw new DatabaseConnectionError(
+  "Some Postgres environment variables weren't found. Please configure them in the .env file."
+);
+let sslmode = process.env.POSTGRES_SSLMODE === "require";
+const connection_string = `postgresql://${process.env.POSTGRES_USERNAME}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB_NAME}?ssl=${sslmode}`;
 const db = pgp(connection_string);
 
 module.exports = db;
